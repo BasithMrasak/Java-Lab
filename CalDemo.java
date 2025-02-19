5db1d09ec7f10f57b4fdb3a84abff00b5ac665dc
BasithMrasak <basithmrasak1@gmail.com>
2025-02-19 14:50:53 +0530

    Implementing arithmetic package

---
+++ CalDemo.java
@@ -0,0 +1,13 @@
+import arithmetic.*;
+public class CalDemo
+{
+    public static void main(String[] args)
+    {
+        Add a=new Add(2,3);
+        Sub s=new Sub(2,3);
+        Mul m=new Mul(2,3);
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(m);
+    }
+}
